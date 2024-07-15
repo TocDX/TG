ba7f5a8a3ce3d9f31561a26886ad4a2468d9cf41
TocDx <sk372021000@gmail.com>
2024-07-15 16:00:31 +0900

    first Project

---
+++ src/main/java/com/tg/vo/AreaBasedListVO.java
@@ -0,0 +1,134 @@
+package com.tg.vo;
+import lombok.Data;
+
+import java.util.List;
+public class AreaBasedListVO {
+    private Response response;
+
+    public Response getResponse() {
+        return response;
+    }
+
+    public void setResponse(Response response) {
+        this.response = response;
+    }
+
+    public static class Response {
+        private Header header;
+        private Body body;
+
+        public Header getHeader() {
+            return header;
+        }
+
+        public void setHeader(Header header) {
+            this.header = header;
+        }
+
+        public Body getBody() {
+            return body;
+        }
+
+        public void setBody(Body body) {
+            this.body = body;
+        }
+    }
+
+    public static class Header {
+        private String resultCode;
+        private String resultMsg;
+
+        public String getResultCode() {
+            return resultCode;
+        }
+
+        public void setResultCode(String resultCode) {
+            this.resultCode = resultCode;
+        }
+
+        public String getResultMsg() {
+            return resultMsg;
+        }
+
+        public void setResultMsg(String resultMsg) {
+            this.resultMsg = resultMsg;
+        }
+    }
+
+    public static class Body {
+        private Items items;
+        private int numOfRows;
+        private int pageNo;
+        private int totalCount;
+
+        public Items getItems() {
+            return items;
+        }
+
+        public void setItems(Items items) {
+            this.items = items;
+        }
+
+        public int getNumOfRows() {
+            return numOfRows;
+        }
+
+        public void setNumOfRows(int numOfRows) {
+            this.numOfRows = numOfRows;
+        }
+
+        public int getPageNo() {
+            return pageNo;
+        }
+
+        public void setPageNo(int pageNo) {
+            this.pageNo = pageNo;
+        }
+
+        public int getTotalCount() {
+            return totalCount;
+        }
+
+        public void setTotalCount(int totalCount) {
+            this.totalCount = totalCount;
+        }
+    }
+
+    public static class Items {
+        private List<Item> item;
+
+        public List<Item> getItem() {
+            return item;
+        }
+
+        public void setItem(List<Item> item) {
+            this.item = item;
+        }
+    }
+    @Data
+    public static class Item {
+        private String addr1;
+        private String addr2;
+        private String areacode;
+        private String booktour;
+        private String cat1;
+        private String cat2;
+        private String cat3;
+        private String contentid;
+        private String contenttypeid;
+        private String createdtime;
+        private String dist;
+        private String firstimage;
+        private String firstimage2;
+        private String cpyrhtDivCd;
+        private String mapx;
+        private String mapy;
+        private String mlevel;
+        private String modifiedtime;
+        private String sigungucode;
+        private String tel;
+        private String title;
+
+
+    }
+}
---
+++ src/main/java/com/tg/vo/DetailIntroVO.java
@@ -0,0 +1,134 @@
+package com.tg.vo;
+import lombok.Data;
+
+import java.util.List;
+public class DetailIntroVO {
+
+    private Response response;
+
+    public Response getResponse() {
+        return response;
+    }
+
+    public void setResponse(Response response) {
+        this.response = response;
+    }
+
+    public static class Response {
+        private Header header;
+        private Body body;
+
+        public Header getHeader() {
+            return header;
+        }
+
+        public void setHeader(Header header) {
+            this.header = header;
+        }
+
+        public Body getBody() {
+            return body;
+        }
+
+        public void setBody(Body body) {
+            this.body = body;
+        }
+    }
+
+    public static class Header {
+        private String resultCode;
+        private String resultMsg;
+
+        public String getResultCode() {
+            return resultCode;
+        }
+
+        public void setResultCode(String resultCode) {
+            this.resultCode = resultCode;
+        }
+
+        public String getResultMsg() {
+            return resultMsg;
+        }
+
+        public void setResultMsg(String resultMsg) {
+            this.resultMsg = resultMsg;
+        }
+    }
+
+    public static class Body {
+        private Items items;
+        private int numOfRows;
+        private int pageNo;
+        private int totalCount;
+
+        public Items getItems() {
+            return items;
+        }
+
+        public void setItems(Items items) {
+            this.items = items;
+        }
+
+        public int getNumOfRows() {
+            return numOfRows;
+        }
+
+        public void setNumOfRows(int numOfRows) {
+            this.numOfRows = numOfRows;
+        }
+
+        public int getPageNo() {
+            return pageNo;
+        }
+
+        public void setPageNo(int pageNo) {
+            this.pageNo = pageNo;
+        }
+
+        public int getTotalCount() {
+            return totalCount;
+        }
+
+        public void setTotalCount(int totalCount) {
+            this.totalCount = totalCount;
+        }
+    }
+
+    public static class Items {
+        private List<Item> item;
+
+        public List<Item> getItem() {
+            return item;
+        }
+
+        public void setItem(List<Item> item) {
+            this.item = item;
+        }
+    }
+    @Data
+    public static class Item {
+        private String contentid;
+        private String contenttypeid;
+        private String sponsor1;
+        private String sponsor1tel;
+        private String sponsor2;
+        private String sponsor2tel;
+        private String eventenddate;
+        private String playtime;
+        private String eventplace;
+        private String eventhomepage;
+        private String agelimit;
+        private String bookingplace;
+        private String placeinfo;
+        private String subevent;
+        private String program;
+        private String eventstartdate;
+        private String usetimefestival;
+        private String discountinfofestival;
+        private String spendtimefestival;
+        private String festivalgrade;
+
+
+    }
+}
